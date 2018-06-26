9fbfd927705b646772c23928eeb32215eb60dada
baneeishaque <baneeishaque@gmail.com>
2018-06-26 14:33:30 +0530

    more

---
+++ file_name_from_path.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+::set S=/name1/name2/name3
+set "S=D:\DK-HP-PA-2000AR\Projects\Apk_Decompiler\Whose Phone Number In Contacts 1.3_4.apk"
+ECHO PATH : %S%
+set I=0
+set L=-1
+:l
+if "!S:~%I%,1!"=="" goto ld
+::if "!S:~%I%,1!"=="/" set L=%I%
+if "!S:~%I%,1!"=="\" set L=%I%
+set /a I+=1
+goto l
+:ld
+REM echo Last Index of \ : %L%
+SET /A L+=1
+::SET SUBSTRING=%S:~0,12%
+CALL SET SUBSTRING=%%S:~%L%%%
+ECHO FILE : %SUBSTRING%
